2b634a8c1b43fd1b2770cbb8ae52ac78b06994d9
dmuiX <store@posteo.de>
2022-10-24 16:06:16 +0200

    Added a new log mechanism

--- SetupComplete/Scripts/SetupComplete.cmd
+++ SetupComplete/Scripts/SetupComplete.cmd
@@ -1,61 +1,64 @@
 @echo off
 
 :: create Temp folder and set log path
-set log=%systemdrive%\SetupComplete.log
+set LogFile=%systemdrive%\%~n0.log
+set logg=^> _^&type _^&type _^>^>%LogFile%
+
+echo TEst this goes to screen AND file! %logg%
 
 set SETUPDIR=%SystemRoot%\Setup\
 set SETUPFILES=%SETUPDIR%\SetupFiles\
 
 :: Install Office 2021 using Office Tool Plus https://help.coolhub.top/others/commands.html#deploy-commands
-echo "Install Office" >> %log
-"%SETUPFILES%\Office Tool\Office Tool Plus.Console.exe" deploy /addProduct O365ProPlusRetail_en-us_Publisher,Lync,Access,OneNote /channel Current /clientEdition 64 /acceptEULA
-echo "Install Office > SUCCESS!" >> %log
+echo "Install Office" >> %logg%
+"%SETUPFILES%\Office Tool\Office Tool Plus.Console.exe" deploy /addProduct O365ProPlusRetail_en-us_Publisher,Lync,Access,OneNote /channel Current /clientEdition 64 /acceptEULA >> %logg%
+if errorlevel 0 ( echo "Install Office > SUCCESS!" >> %logg% )
 
 :: Activate Office and Windows at this place using certain tools
-echo "Activate Office" >> %log
-start /wait cmd /c %SETUPDIR%\Scripts\activateOffice.cmd
-echo "Activate Office > SUCCESS!" >> %log
+echo "Activate Office" >> %logg%
+start /wait cmd /c %SETUPDIR%\Scripts\activateOffice.cmd >> %logg%
+if errorlevel 0 ( echo "Activate Office > SUCCESS!" >> %logg% )
 
 :: Add some registry entries
-echo "Add registry entries" >> %log
-reg add "HKLM\Software\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsConsumerFeatures /d 1 /t REG_DWORD /f
-reg add "HKCU\Software\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsConsumerFeatures /d 1 /t REG_DWORD /f
-reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v ContentDeliveryAllowed /d 0 /t REG_DWORD /f
-reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SilentInstalledAppsEnabled /d 0 /t REG_DWORD /f
-reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager\" /v SystemPaneSuggestionsEnabled /d 0 /t REG_DWORD /f
-echo "Add registry entries > SUCCESS!" >> %log
+echo "Add registry entries" >> %logg%
+reg add "HKLM\Software\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsConsumerFeatures /d 1 /t REG_DWORD /f >> %logg%
+reg add "HKCU\Software\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsConsumerFeatures /d 1 /t REG_DWORD /f >> %logg%
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v ContentDeliveryAllowed /d 0 /t REG_DWORD /f >> %logg%
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SilentInstalledAppsEnabled /d 0 /t REG_DWORD /f >> %logg%
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager\" /v SystemPaneSuggestionsEnabled /d 0 /t REG_DWORD /f >> %logg%
+if errorlevel 0 ( echo "Add registry entries > SUCCESS!" >> %logg% )
 
 :: Run Decrapifier Script
-echo "Run Decrapifier Script" >> %log
+echo "Run Decrapifier Script" >> %logg%
 SET PowerShellScriptPath=%SETUPDIR%\Scripts\decrapifier.ps1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File """"%PowerShellScriptPath%"""" -ClearStart' -Verb RunAs;}"
 timeout 360
-echo "Run Decrapifier Script > SUCCESS" >> %log
+if errorlevel 0 ( echo "Run Decrapifier Script > SUCCESS" >> %logg% )
 
 :: Deinstall OneDrive
-echo "Deinstall OneDrive" >> %log
+echo "Deinstall OneDrive" >> %logg%
 taskkill /f /im OneDrive.exe
-%SystemRoot%\SysWOW64\OneDriveSetup.exe /uninstall
-echo "Deinstall OneDrive > SUCCESS" >> %log
+%SystemRoot%\SysWOW64\OneDriveSetup.exe /uninstall >> %logg%
+if errorlevel 0 ( echo "Deinstall OneDrive > SUCCESS" >> %logg% )
 
 :: Deinstall Teams
-echo "Deinstall Teams" >> %log
-wmic product where name="Teams Machine-Wide Installer" call uninstall
-echo "Deinstall Teams > SUCCESS" >> %log
+echo "Deinstall Teams" >> %logg%
+wmic product where name="Teams Machine-Wide Installer" call uninstall >> %logg%
+if errorlevel 0 ( echo "Deinstall Teams > SUCCESS" >> %logg% )
 
 :: Install Apps using scoop
-echo "Install Apps using scoop" >> %log
-start /wait cmd /c %SETUPDIR%\Scripts\scoop.cmd
-echo "Install Apps using scoop > SUCCESS" >> %log
+echo "Install Apps using scoop" >> %logg%
+start /wait cmd /c %SETUPDIR%\Scripts\scoop.cmd >> %logg%
+if errorlevel 0 ( echo "Install Apps using scoop > SUCCESS" >> %logg% )
 
 :: Install Apps using Choco
 :: start /wait cmd /c %SETUPDIR%\Scripts\choco.cmd
 
 :: Clean up
 :: if exist "%SETUPDIR%\Ninite.exe" @DEL /S /Q "%SETUPDIR%\Ninite.exe"
-if exist "%SETUPFILES%" @RD /S /Q "%SETUPFILES%"
-if exist "%SETUPDIR%\PreSetup\" @RD /S /Q "%SETUPDIR%\PreSetup\"
-if exist "%SETUPDIR%\Scripts\" @RD /S /Q "%SETUPDIR%\Scripts\"
+:: if exist "%SETUPFILES%" @RD /S /Q "%SETUPFILES%"
+:: if exist "%SETUPDIR%\PreSetup\" @RD /S /Q "%SETUPDIR%\PreSetup\"
+:: if exist "%SETUPDIR%\Scripts\" @RD /S /Q "%SETUPDIR%\Scripts\"
 
 ::Reboot
 shutdown /r /t 15
--- SetupComplete/Scripts/scoop.cmd
+++ SetupComplete/Scripts/scoop.cmd
@@ -3,27 +3,29 @@ echo %path%|find /i "scoop" > NUL
 if %errorlevel% == 0 (
 
 :: md %systemdrive%\Temp > nul
-set log=%systemdrive%\%~n0.log
+:: set log=%systemdrive%\%~n0.log
+set LogFile=%systemdrive%\%~n0.log
+set logg=^> _^&type _^&type _^>^>%LogFile
 
-scoop install git >> %log%
-scoop bucket add extras >> %log%
-scoop bucket add main >> %log%
+scoop install git >> %logg%
+scoop bucket add extras >> %logg%
+scoop bucket add main >> %logg%
 
-scoop install sudo >> %log%
-scoop install clink >> %log%
-clink autorun install >> %log%
-scoop install firefox >> %log%
-scoop install brave >> %log%
-scoop install windows-terminal >> %log%
+scoop install sudo >> %logg%
+scoop install clink >> %logg%
+clink autorun install >> %logg%
+scoop install firefox >> %logg%
+scoop install brave >> %logg%
+scoop install windows-terminal >> %logg%
 
-scoop bucket add anderlli0053_DEV-tools https://github.com/anderlli0053/DEV-tools >> %log%
-scoop install HashTab >> %log%
-scoop install autodarkmode >> %log%
-scoop install itunes-portable >> %log%
-scoop install mediainfo >> %log%
-scoop install advancedrenamer >> %log%
-scoop install vscode >> %log%
-scoop install treesize-free >> %log%
-scoop install defraggler >> %log%
-scoop install vmware-workstation-pro >> %log%
+scoop bucket add anderlli0053_DEV-tools https://github.com/anderlli0053/DEV-tools >> %logg%
+scoop install HashTab >> %logg%
+scoop install autodarkmode >> %logg%
+scoop install itunes-portable >> %logg%
+scoop install mediainfo >> %logg%
+scoop install advancedrenamer >> %logg%
+scoop install vscode >> %logg%
+scoop install treesize-free >> %logg%
+scoop install defraggler >> %logg%
+scoop install vmware-workstation-pro >> %logg%
 )
